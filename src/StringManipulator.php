86992023cd8cd0820999d64253aa3f525b27fa7f
CaioBalc <balczarekcaio@gmail.com>
2024-02-15 22:05:47 -0300

    Exercício 3

--- src/StringManipulator.php
+++ src/StringManipulator.php
@@ -1,2 +1,11 @@
 <?php
 
+class StringManipulator{
+    function capitalizeString($string){
+        return ucfirst($string);
+    }
+
+    public function concatenateStrings($string1, $string2) {
+        return $string1 . $string2;
+    }
+}
--- tests/StringManipulatorTest.php
+++ tests/StringManipulatorTest.php
@@ -1,2 +1,23 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
+
+require_once "src/StringManipulator.php";
+
+class StringManipulatorTest extends TestCase{
+    public function testCapitalizeStringVazia() {
+        $stringManipulator = new StringManipulator();
+
+        $result = $stringManipulator->capitalizeString('');
+
+        $this->assertEquals('', $result);
+    }
+
+    public function testConcatenateStringsComEspaco() {
+        $stringManipulator = new StringManipulator();
+
+        $result = $stringManipulator->concatenateStrings('Hello', ' World');
+
+        $this->assertEquals('Hello World', $result);
+    }
+}
